9354cbda5469260592df4d0b04342e62df6c1fd4
Alexander Pelta <alexpxrad@gmail.com>
2022-08-11 14:58:22 -0400

    instruction on how to use context

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+
+import { useContext } from "react";
+import { UserInfoContext } from "../App";
+
+
+
+export default function Login() {
+    const { setUser, setIsLoggedIn } = useContext(UserInfoContext)
+    const handleLogin = () => {
+        setIsLoggedIn(true)
+        setUser({name: 'Jonathan', position: 'Drum', band: 'Lane Branden'})
+
+    }
+    return <button onClick={handleLogin}>Login</button> 
+}
